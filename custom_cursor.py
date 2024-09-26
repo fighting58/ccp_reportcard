c566d2e346a8a0ea4a48aa0af5c7ecaf5c3b7cc5
fighting58 <40029469+fighting58@users.noreply.github.com>
2024-09-26 23:44:38 +0900

    Delete custom_cursor.py

--- custom_cursor.py
+++
@@ -1,7 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QCursor, QPixmap
-
-app = QApplication(sys.argv)
-pen_cursor = QCursor(QPixmap('images/pen_cursor.png'), hotX=0, hotY=32)
-text_cursor = QCursor(QPixmap('images/text_cursor.png'))
